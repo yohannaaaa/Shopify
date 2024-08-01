244a17ed2e38e41646504a8494e0a743be76e70b
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2024-08-01 21:33:18 +0300

    Edits

--- shopify/app/edit-profile/loading.tsx
+++ shopify/app/edit-profile/loading.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function GlobalSuspense() {
+export default function ProfileSuspense() {
   return (
   <div className="animate-spin text-8xl">
     <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
