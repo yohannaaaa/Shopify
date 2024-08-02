e18f5573093ba56f8d5aa948d5edeeda9df0700c
yohannaaaa <yohannamerawi@gmail.com>
2024-08-02 12:21:45 +0300

    lanidng-update

--- shopify/app/page.tsx
+++ shopify/app/page.tsx
@@ -11,24 +11,25 @@ const LandingPage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
     
-
+   
       <main className="flex-grow container mx-auto p-4 text-center">
+        
         <h1 className="text-4xl font-bold mb-4">Welcome to Shopify</h1>
         <p className="text-lg mb-8">Your one-stop shop for the latest in electronic devices.</p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
-          <div className="bg-white shadow-lg rounded-lg p-4">
-            <Image src="/Laptop.jpg" alt="Laptop" width={400} height={300} className="rounded-lg mb-4"/>
+          <div className="bg-white shadow-lg rounded-lg p-2">
+            <Image src="/RoundMini.webp" alt="Laptop" width={400} height={300} className="rounded-lg mb-4"/>
             <h2 className="text-xl font-semibold">Laptops</h2>
             <p className="text-gray-600">Discover the latest laptops from top brands.</p>
           </div>
-          <div className="bg-white shadow-lg rounded-lg p-4">
+          <div className="bg-white shadow-lg rounded-lg p-2">
             <Image src="/PantonyPhone.webp" alt="Smartphone" width={400} height={300} className="rounded-lg mb-4"/>
             <h2 className="text-xl font-semibold">Smartphones</h2>
             <p className="text-gray-600">Find the perfect smartphone for your needs.</p>
           </div>
-          <div className="bg-white shadow-lg rounded-lg p-4">
-            <Image src="/Airpod-max.jpg" alt="Headphones" width={400} height={300} className="rounded-lg mb-4"/>
+          <div className="bg-white shadow-lg rounded-lg p-2">
+            <Image src="/SmartWatch.webp" alt="Headphones" width={400} height={300} className="rounded-lg mb-4"/>
             <h2 className="text-xl font-semibold">Headphones</h2>
             <p className="text-gray-600">Experience high-quality sound with our headphones.</p>
           </div>
